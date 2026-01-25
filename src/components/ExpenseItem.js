28f4cb5fe2a52b44cf2350124227863a2444bb57
Priyanka Sah <Priyanka.Sah1@ibm.com>
2026-01-25 23:12:28 +0530

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(){
+    return (
+        <div className='expense-item'>
+            <div>Jan 25th 2025 </div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$200</div>
+            </div>
+        </div>
+    );
+
+}
+
+export default ExpenseItem;
