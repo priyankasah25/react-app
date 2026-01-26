19bb716141c38079c5cc629fe2b70c28e4b3ca12
Priyanka Sah <Priyanka.Sah1@ibm.com>
2026-01-26 12:34:36 +0530

    feat: Same component used multiple time and fix comment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App(){
-    let expenseDate = new Date(2026, 1, 26);
-    let expenseTitle = "School Fess";
-    let expenseAmount = 300;
+
+    let expenses = [
+        {
+            id: 'e1',
+            date: new Date(2026,1,26),
+            title: "School Fess",
+            amount: 300
+        },
+
+        {
+            id: 'e2',
+            date: new Date(2026,1,27),
+            title: "College Fess",
+            amount: 350
+        },
+
+        {
+            id: 'e3',
+            date: new Date(2026,1,28),
+            title: "Rental Fess",
+            amount: 400
+        },
+
+        {
+            id: 'e4',
+            date: new Date(2026,1,29),
+            title: "Vehical Fess",
+            amount: 500
+        }
+    ];
+
     return (
+        <div>
         <ExpenseItem
-            date = {expenseDate}
-            title = {expenseTitle}
-            amount = {expenseAmount}
+            date = {expenses[0].date}
+            title = {expenses[0].title}
+            amount = {expenses[0].amount}
         />
+
+        <ExpenseItem
+            date = {expenses[1].date}
+            title = {expenses[1].title}
+            amount = {expenses[1].amount}
+        />
+
+        <ExpenseItem
+            date = {expenses[2].date}
+            title = {expenses[2].title}
+            amount = {expenses[2].amount}
+        />
+
+        <ExpenseItem
+            date = {expenses[3].date}
+            title = {expenses[3].title}
+            amount = {expenses[3].amount}
+        />
+        </div>
     );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -12,7 +12,8 @@ function ExpenseItem(props){
                 <div>{ day }</div>
             </div>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2> // jo data recieve hua hai direct use kar sakte or else make changes like done in date thing (means data manipulate karlo and use it)
+                {/*jo data recieve hua hai direct use kar sakte or else make changes like done in date thing (means data manipulate karlo and use it)*/}
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
