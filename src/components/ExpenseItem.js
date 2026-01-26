f7d0289909edd8d2bf6b905ee1f669491380867b
Priyanka Sah <Priyanka.Sah1@ibm.com>
2026-01-26 11:56:40 +0530

    fix: Updated the way Date was written(better way)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props){
+    const month = props.date.toLocaleString('en-US', {month: 'long'}) // toLocaleString -> date is object ushka method is toLocaleString
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'})
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{ month }</div>
+                <div>{ year }</div>
+                <div>{ day }</div>
+            </div>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{props.title}</h2> // jo data recieve hua hai direct use kar sakte or else make changes like done in date thing (means data manipulate karlo and use it)
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
