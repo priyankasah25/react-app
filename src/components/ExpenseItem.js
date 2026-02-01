c3bb0f066084b6cc7af1267d4505da4d07e94ba8
Priyanka Sah <Priyanka.Sah1@ibm.com>
2026-02-01 16:38:12 +0530

    feat: Pitting one component inside other (Card.js)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem(props){
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={ props.date }/>
             <div className='expense-item__description'>
                 {/*jo data recieve hua hai direct use kar sakte or else make changes like done in date thing (means data manipulate karlo and use it)*/}
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 
 function Expenses(props) {
 
     return(
-        <div className='expenses'>
+        <Card className='expenses'>
         <ExpenseItem
             date = {props.item[0].date}
             title = {props.item[0].title}
@@ -29,7 +30,7 @@ function Expenses(props) {
             title = {props.item[3].title}
             amount = {props.item[3].amount}
         />
-        </div>
+        </Card>
 
     );
 
